0e4924b9ed32297f3d863b1948f90f4cfb4375f8
hajaemin <woals3527@naver.com>
2024-11-16 08:58:35 +0900

    feat: 상품카테고리

---
+++ src/pages/product/ProductCategoryPage.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import ProductCategoryComponent from "../../components/product/ProductCategoryComponent.vue";
+</script>
+
+<template>
+  <div>
+    <ProductCategoryComponent></ProductCategoryComponent>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
