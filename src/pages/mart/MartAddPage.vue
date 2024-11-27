5e2e846179f6497876732e0adb07a7b31736da9b
hajaemin <woals3527@naver.com>
2024-11-27 18:05:08 +0900

    feat: 마트 변경

---
+++ src/pages/mart/MartAddPage.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import MartAddComponent from "../../components/mart/MartAddComponent.vue";
+</script>
+
+<template>
+  <div>
+    <MartAddComponent></MartAddComponent>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/pages/mart/MartListPage.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import MartListComponent from "../../components/mart/MartListComponent.vue";
+</script>
+
+<template>
+  <div>
+    <MartListComponent></MartListComponent>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
